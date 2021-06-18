7f451df867f7b12254872bb2ee97ada0c3598c78
José Simões <jose.simoes@eclo.solutions>
2021-06-18 17:59:36 +0100

    Code refactoring
    - Rework comments for documentation.
    - Minor fixes from code revision and analysers.
    - Fix typo in file name.

--- System.IO.Ports/SerialDeviceEventListener.cs
+++ System.IO.Ports/SerialDeviceEventListener.cs
@@ -11,7 +11,7 @@ namespace System.IO.Ports
     internal class SerialDeviceEventListener : IEventProcessor, IEventListener
     {
         // Map of serial device numbers to SerialDevice objects.
-        private readonly ArrayList _serialDevicesMap = new ArrayList();
+        private readonly ArrayList _serialDevicesMap = new();
 
         public SerialDeviceEventListener()
         {
