184ce0c484dcee57024a77e1b3faf1ca599539e6
José Simões <jose.simoes@eclo.solutions>
2022-09-15 18:36:17 +0100

    Bump AssemblyNativeVersion to 100.1.6.1
    - Was missing from #102.
    
    ***NO_CI***

--- System.IO.Ports/Properties/AssemblyInfo.cs
+++ System.IO.Ports/Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -15,7 +14,7 @@ using System.Runtime.InteropServices;
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
-[assembly: AssemblyNativeVersion("100.1.6.0")]
+[assembly: AssemblyNativeVersion("100.1.6.1")]
 ////////////////////////////////////////////////////////////////
 
 // Setting ComVisible to false makes the types in this assembly not visible 
